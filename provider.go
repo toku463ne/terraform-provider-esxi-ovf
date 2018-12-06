cb95160b94bd94d60965029e454481a9650537fc
Mine Tokushiro <t_mine@gmail.co.jp>
2018-12-06 19:24:57 +0900

    Added VM to provider list

--- provider.go
+++ provider.go
@@ -8,7 +8,8 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
-			"ovfdeployer_pool": resourcePool(),
+			"esxi-ovf_pool": resourcePool(),
+			"esxi-ovf_vm": resourceVM(),
 		},
 	}
 }
